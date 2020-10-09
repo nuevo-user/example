c4bd0afd4cab5ca0611924ae9f2377172c9e75a7
AlucardG <alanwalker199700@hotmail.com>
2020-10-08 23:00:47 -0500

    reiniciando despued de juntar dos ramas y malograr todo pff practica mas como unir una rama con la rama master

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const mytitle = document.querySelector('div')
+
+
+mytitle.style.color = 'white';
+mytitle.style.backgroundColor = 'black';
+mytitle.style.padding = '10px';
+mytitle.style.width = '250px';
+mytitle.style.textAlign = 'center';
