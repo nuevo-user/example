6d68cdbe0443d312a8bb367bd1038e57c90254df
AlucardG <alanwalker199700@hotmail.com>
2020-10-11 15:19:27 -0500

    ajustes de hover de javascript

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-const mytitle = document.querySelector('h3');
+const mytitle = document.querySelector('h1');
 
 mytitle.addEventListener('mouseover', mouseOver)
 mytitle.addEventListener('mouseout', mouseOut)
 
 function mouseOver() {
-    mytitle.style.color = "red";
+    mytitle.style.color = "white";
 }
 function mouseOut() {
     mytitle.style.color = "black";
